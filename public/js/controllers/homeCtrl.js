11e07c5d18487e91b80650adf264292861452bb7
jonnywilhite <jonnygwilhite@gmail.com>
2016-09-17 21:52:53 -0400

    user can cycle through each flashcard with a button click

--- public/js/controllers/homeCtrl.js
+++ public/js/controllers/homeCtrl.js
@@ -4,6 +4,12 @@ angular.module('app').controller('homeCtrl', function($http, $location, $cookies
   var homeCtrlData = this;
 
   homeCtrlData.flashcards = [];
+  homeCtrlData.fcIndex = 0;
+  homeCtrlData.flashcardDisplayed = false;
+  homeCtrlData.questionDisplayed = false;
+  homeCtrlData.answerDisplayed = false;
+  homeCtrlData.currentQuestion = "";
+  homeCtrlData.currentAnswer = "";
   homeCtrlData.username = (AuthService.currentUser() && AuthService.currentUser().username) || $cookies.get('username');
 
   $http.get('/api/flashcards')
@@ -13,6 +19,29 @@ angular.module('app').controller('homeCtrl', function($http, $location, $cookies
     .error(function(data) {
       console.log('Error getting flashcards: ' + data);
     });
+
+  homeCtrlData.showQuestion = function () {
+    if (homeCtrlData.fcIndex == homeCtrlData.flashcards.length) {
+      homeCtrlData.flashcardDisplayed = false;
+      homeCtrlData.questionDisplayed = false;
+      homeCtrlData.answerDisplayed = false;
+      homeCtrlData.currentQuestion = "";
+      homeCtrlData.currentAnswer = "";
+      homeCtrlData.fcIndex = 0;
+    } else {
+      homeCtrlData.flashcardDisplayed = true;
+      homeCtrlData.answerDisplayed = false;
+      homeCtrlData.questionDisplayed = true;
+      homeCtrlData.currentQuestion = homeCtrlData.flashcards[homeCtrlData.fcIndex].question;
+    }
+  };
+
+  homeCtrlData.showAnswer = function () {
+    homeCtrlData.questionDisplayed = false;
+    homeCtrlData.answerDisplayed = true;
+    homeCtrlData.currentAnswer = homeCtrlData.flashcards[homeCtrlData.fcIndex].answer;
+    homeCtrlData.fcIndex++;
+  };
 
   homeCtrlData.logout = function () {
     $http.get('/logout')
@@ -25,4 +54,5 @@ angular.module('app').controller('homeCtrl', function($http, $location, $cookies
         $location.path('/logout');
       });
   };
+
 });
